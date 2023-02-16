0a82e9d7826e75b9a515913b339140e24ae889fc
Pak-Piki <pakpiki66@gmail.com>
2023-02-16 21:53:33 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#84cc16',
+      },
+    },
+  },
+  plugins: [],
+};
